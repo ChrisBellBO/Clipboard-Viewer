c8e8d38ae638eedba667bb7567f706840dfd1573
Chris Bell <chris.bell@businessoptix.com>
2024-12-24 19:49:36 +0000

    version bump

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Chris Bell")]
 [assembly: AssemblyProduct("Clipboard Viewer")]
-[assembly: AssemblyCopyright("Copyright © Chris Bell 2011")]
+[assembly: AssemblyCopyright("Copyright © Chris Bell 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
 
@@ -26,7 +26,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyVersion("1.0.0.3")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
